7612bca79105f802917c90137a2add93d865516f
kotamori9 <kotamori27@gmail.com>
2020-03-03 02:54:36 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chatspacemorinaka'
+  config.fog_directory  = chatspacemorinaka
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
 end
